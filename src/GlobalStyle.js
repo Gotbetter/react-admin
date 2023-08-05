a1786934b0b880dd8d0d8d2e72c2c5987f721134
cofls980 <qawse980@gmail.com>
2023-08-05 23:12:06 +0900

    [feat] 폰트 적용

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,11 +4,18 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
   }
+  @font-face {
+      font-family: 'Pretendard-Regular';
+      src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
+      font-weight: 700;
+      font-style: normal;
+  }
   body, html {
     /* height: 100%; */
     margin: 0;
     padding: 0;
     background-color: #363740;
+    font-family: "Pretendard-Regular";
   }
   /* html {
     font-size: 10px;
