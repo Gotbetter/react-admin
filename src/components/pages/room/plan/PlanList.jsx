d68700952c8240df618e8f18495489fe1a7e1747
cofls980 <qawse980@gmail.com>
2023-08-13 18:37:52 +0900

    [feat] 한 주차 계획에 대한 평가자 추가 기능 구현

--- src/components/pages/room/plan/PlanList.jsx
+++ src/components/pages/room/plan/PlanList.jsx
@@ -19,14 +19,14 @@ import { useApiError } from "../../../../api/useApiError";
 import PlanInfoModal from "./PlanInfoModal";
 import PlanDislikeList from "./PlanDislikeList";
 import AddDetailPlanModal from "./AddDetailPlanModal";
+import AddPlanDislikeModal from "./AddPlanDislikeModal";
 
 export default function PlanList({ handleParticipantClick, participant_id }) {
   const [selectedOption, setSelectedOption] = useState(null);
   const [planList, setPlanList] = useState(null);
   const [clickInfoModal, setClickInfoModal] = useState(false);
-  const [clickAddModal, setClickAddModal] = useState(false);
-
-  const queryClient = useQueryClient();
+  const [clickAddDetailModal, setClickAddDetailModal] = useState(false);
+  const [clickAddDislikeModal, setClickAddDislikeModal] = useState(false);
 
   const errorhandling = useErrorHandling();
   const { handleError } = useApiError(undefined, errorhandling);
@@ -45,8 +45,12 @@ export default function PlanList({ handleParticipantClick, participant_id }) {
     }
   );
 
-  const handleClickAddModal = () => {
-    setClickAddModal(!clickAddModal);
+  const handleClickAddDetailModal = () => {
+    setClickAddDetailModal(!clickAddDetailModal);
+  };
+
+  const handleClickAddDislikeModal = () => {
+    setClickAddDislikeModal(!clickAddDislikeModal);
   };
 
   const handleClickInfoModal = () => {
@@ -92,10 +96,12 @@ export default function PlanList({ handleParticipantClick, participant_id }) {
         {clickInfoModal && (
           <PlanInfoModal
             handleClickInfoModal={handleClickInfoModal}
-            planInfo={selectedOption}
+            planInfo={planList.find(
+              (plan) => plan.plan_id === selectedOption.plan_id
+            )}
           />
         )}
-        <AddModalButton onClick={handleClickAddModal}>
+        <AddModalButton onClick={handleClickAddDetailModal}>
           상세 계획 추가
         </AddModalButton>
       </ArrowWrapper>
@@ -104,7 +110,12 @@ export default function PlanList({ handleParticipantClick, participant_id }) {
           <DetailPlanList planId={selectedOption.plan_id}></DetailPlanList>
         )}
       </GraphTemplate>
-      <MiddleTitle>전체 계획에 대해 싫어요 누른 멤버</MiddleTitle>
+      <MiddleTitle>
+        전체 계획에 대해 싫어요 누른 멤버
+        <AddModalButton onClick={handleClickAddDislikeModal}>
+          싫어요 누를 멤버 추가
+        </AddModalButton>
+      </MiddleTitle>
       <GraphTemplate
         columns={plan_dislike_columns}
         paddings={plan_dislike_paddings}
@@ -113,10 +124,17 @@ export default function PlanList({ handleParticipantClick, participant_id }) {
           <PlanDislikeList planId={selectedOption.plan_id}></PlanDislikeList>
         )}
       </GraphTemplate>
-      {clickAddModal && (
+      {clickAddDetailModal && (
         <AddDetailPlanModal
-          handleClickAddModal={handleClickAddModal}
+          handleClickAddModal={handleClickAddDetailModal}
           planId={selectedOption.plan_id}
+        />
+      )}
+      {clickAddDislikeModal && (
+        <AddPlanDislikeModal
+          handleClickModal={handleClickAddDislikeModal}
+          planId={selectedOption.plan_id}
+          queryKey='plans'
         />
       )}
     </>
@@ -138,7 +156,6 @@ const ArrowWrapper = styled.div`
 const IconButton = styled.img`
   width: 18px;
   height: 18px;
-  margin-right: 10px;
   cursor: pointer;
 `;
 
@@ -159,6 +176,7 @@ const MiddleTitle = styled.div`
 
 const Select = styled.select`
   width: 250px;
+  margin-left: 10px;
   margin-right: 10px;
   padding: 3px;
   font-size: 14px;
@@ -172,6 +190,14 @@ const Select = styled.select`
 `;
 
 const AddModalButton = styled.div`
+  align-items: center;
+  margin-left: 10px;
+  font-size: 17px;
+  font-style: normal;
+  font-weight: normal;
+  line-height: normal;
+  letter-spacing: 0.3px;
+  color: #9fa2b4;
   cursor: pointer;
   transition: color 0.3s; /* 변화를 부드럽게 만들기 위한 트랜지션 설정 */
 
