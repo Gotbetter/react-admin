9e91e2af6d4c230f262337f2afe935a3dc3c250a
cofls980 <qawse980@gmail.com>
2023-08-14 00:32:40 +0900

    [feat] 방 관리의 세부 계획 인증 삭제 기능 구현

--- src/api/detailPlanRecord.js
+++ src/api/detailPlanRecord.js
@@ -9,6 +9,11 @@ export const createDetailRecord = (detailPlanId, recordInfo) =>
 export const updateDetailRecord = (detailPlanId, recordId, recordInfo) =>
   client.patch(`details/${detailPlanId}/records/${recordId}/admin`, recordInfo);
 
+export const deleteDetailPlanRecord = (detailPlanId, recordId, admin) =>
+  client.delete(`details/${detailPlanId}/records/${recordId}`, {
+    params: { admin },
+  });
+
 export const fetchDetailDislikes = (detailPlanId) =>
   client.get(`details/${detailPlanId}/dislike/admin`);
 
